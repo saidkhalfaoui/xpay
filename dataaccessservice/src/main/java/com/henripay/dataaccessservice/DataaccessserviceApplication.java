f36c8eab79392be1734a0433c4cb090370272867
Tarek <t_lacheheb@esi.dz>
2024-03-09 13:58:53 +0100

    HEN-2

---
+++ dataaccessservice/src/main/java/com/henripay/dataaccessservice/DataaccessserviceApplication.java
@@ -0,0 +1,13 @@
+package com.henripay.dataaccessservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataaccessserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataaccessserviceApplication.class, args);
+	}
+
+}
---
+++ dataaccessservice/src/test/java/com/henripay/dataaccessservice/DataaccessserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henripay.dataaccessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataaccessserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
