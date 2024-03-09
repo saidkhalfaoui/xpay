cf890adc30eee0c4245dc49c3508065533e668f6
Tarek <t_lacheheb@esi.dz>
2024-03-09 14:27:49 +0100

    HEN-2

--- common/src/main/java/com/henripay/common/exception/HenripayExceptionHandler.java
+++ common/src/main/java/com/henripay/common/exception/HenripayExceptionHandler.java
@@ -1,10 +1,32 @@
 package com.henripay.common.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@ControllerAdvice
 public class HenripayExceptionHandler {
-//    public ResponseEntity<Object> handleHenripayRequestException(HenripayRequestException e) {
-//        new HenripayException(e.getMessage(),
-//                e,
-//                HttpStatus.BAD_REQUEST,
-//                ZonedDateTime.now(ZoneId.of("Z")));
-//    }
+    @ExceptionHandler(value = {HenripayRequestException.class})
+    public ResponseEntity<Object> handleHenripayBadRequestException(HenripayRequestException e) {
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        HenripayException henripayException = new HenripayException(e.getMessage(),
+                e,
+                badRequest,
+                ZonedDateTime.now(ZoneId.of("Z")));
+        return new ResponseEntity<>(henripayException, badRequest);
+    }
+
+    @ExceptionHandler(value = {HenripayRequestException.class})
+    public ResponseEntity<Object> handleHenripayNotFoundException(HenripayRequestException e) {
+        HttpStatus notFound = HttpStatus.NOT_FOUND;
+        HenripayException henripayException = new HenripayException(e.getMessage(),
+                e,
+                notFound,
+                ZonedDateTime.now(ZoneId.of("Z")));
+        return new ResponseEntity<>(henripayException, notFound);
+    }
 }
