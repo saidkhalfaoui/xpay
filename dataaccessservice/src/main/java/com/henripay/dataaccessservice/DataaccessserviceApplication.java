e18f4928352786bae682fec62513cabd75e2b9fd
Tarek <t_lacheheb@esi.dz>
2024-03-09 14:12:54 +0100

    HEN-2

--- dataaccessservice/src/main/java/com/henripay/dataaccessservice/DataaccessserviceApplication.java
+++ dataaccessservice/src/main/java/com/henripay/dataaccessservice/DataaccessserviceApplication.java
@@ -2,8 +2,11 @@ package com.henripay.dataaccessservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.henripay.domainservice.entity")
+
 public class DataaccessserviceApplication {
 
 	public static void main(String[] args) {
