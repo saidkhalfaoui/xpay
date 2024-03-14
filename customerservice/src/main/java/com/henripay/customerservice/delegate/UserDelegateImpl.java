a05111ac58d48fd236e19224993e1555389f7688
Abdulrahim Djebablia <ADjebablia@aas.com.sa>
2024-03-14 17:54:29 +0100

    feat: add openapi specification for user service APIs

--- customerservice/src/main/java/com/henripay/customerservice/delegate/UserDelegateImpl.java
+++ customerservice/src/main/java/com/henripay/customerservice/delegate/UserDelegateImpl.java
@@ -1,33 +1,12 @@
 package com.henripay.customerservice.delegate;
 
-import com.henripay.customerservice.controller.UserApiDelegate;
-import com.henripay.customerservice.dto.UserDTO;
-import com.henripay.customerservice.service.UserService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 /*
  *  @author: DevProblems(Sarang Kumar A Tak)
  *  Youtube channel: https://youtube.com/@devproblems
  */
 @Service
-public class UserDelegateImpl implements UserApiDelegate {
+public class UserDelegateImpl  {
 
-    private final UserService userService;
-
-    public UserDelegateImpl(UserService userService) {
-        this.userService = userService;
-    }
-
-    @Override
-    public ResponseEntity<UserDTO> saveUser(UserDTO userDTO) {
-        return ResponseEntity.ok(userService.saveUser(userDTO));
-    }
-
-    @Override
-    public ResponseEntity<UserDTO> getUserById(UUID id) {
-        return ResponseEntity.ok(userService.getUser(id));
-    }
 }
---
+++ domainservice/src/main/java/com/henripay/domainservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.henripay.domainservice.repository;
+
+import com.henripay.domainservice.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
