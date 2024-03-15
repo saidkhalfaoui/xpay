1cc02acd984626a6c07e1f19d39271e995e7ab9d
Tarek <t_lacheheb@esi.dz>
2024-03-15 18:25:12 +0100

    HEN-2

--- customerservice/src/test/java/com/henripay/customerservice/CutomerserviceApplicationTests.java
+++ customerservice/src/test/java/com/henripay/customerservice/CutomerserviceApplicationTests.java
@@ -1,23 +1,14 @@
 package com.henripay.customerservice;
 
-import com.henripay.customerservice.tests.TestDataService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CutomerserviceApplicationTests {
 
-    @Autowired
-    private TestDataService testDataService;
 
     @Test
     public void testSaveAndPrintData() {
-        // Save test data
-        testDataService.saveData();
-
-        // Print all saved data
-        testDataService.printAllData();
     }
 
 }
