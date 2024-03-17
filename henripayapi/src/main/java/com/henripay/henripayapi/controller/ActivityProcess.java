45b4a6f02172e15cc381593194ad3b3dc67f45ae
Mohammed Naami <m.naami@outlook.com>
2024-03-17 18:15:07 +0100

    HEN-1-t2

--- henripayapi/src/main/java/com/henripay/henripayapi/controller/ActivityProcess.java
+++ henripayapi/src/main/java/com/henripay/henripayapi/controller/ActivityProcess.java
@@ -3,9 +3,11 @@ package com.henripay.henripayapi.controller;
 import com.henripay.henripayapi.service.CommandService;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.community.rest.client.invoker.ApiException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @RestController
@@ -23,6 +25,20 @@ public class ActivityProcess {
             @PathVariable String key
     ) throws ApiException {
         commandService.startProcess(key);
+    }
+
+    @PostMapping("/process-2/start/{key}")
+    public ResponseEntity<String> startProcess2(
+            @PathVariable String key
+    ) throws ApiException {
+        return commandService.startProcess2(key);
+    }
+
+    @PostMapping("/process-mono/start/{key}")
+    public Mono<ResponseEntity<String>> startProcessMono(
+            @PathVariable String key
+    ) throws ApiException {
+        return commandService.startProcessMono(key);
     }
 
     @PostMapping("/process/send-message/{instanceId}/{messageName}")
--- henripayapi/src/main/java/com/henripay/henripayapi/service/CommandService.java
+++ henripayapi/src/main/java/com/henripay/henripayapi/service/CommandService.java
@@ -1,12 +1,8 @@
 package com.henripay.henripayapi.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.camunda.bpm.client.ExternalTaskClient;
-import org.camunda.bpm.client.task.ExternalTaskService;
 import org.camunda.community.rest.client.api.ProcessDefinitionApi;
 import org.camunda.community.rest.client.api.ProcessInstanceApi;
-import org.camunda.community.rest.client.dto.ProcessInstanceDto;
-import org.camunda.community.rest.client.dto.ProcessInstanceWithVariablesDto;
 import org.camunda.community.rest.client.dto.StartProcessInstanceDto;
 import org.camunda.community.rest.client.dto.VariableValueDto;
 import org.camunda.community.rest.client.invoker.ApiException;
@@ -15,11 +11,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.MonoSink;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static java.time.Duration.ofMillis;
+import static java.time.Duration.ofSeconds;
 
 @Slf4j
 @Transactional(rollbackFor = Exception.class)
@@ -59,9 +64,8 @@ public class CommandService {
         while (step1 == null || !(Objects.equals(step1.getValue(), "C") || Objects.equals(step1.getValue(), "F"))) {
             try {
                 Thread.sleep(1000);
-                //var ps = processInstanceApi.getProcessInstance(processInstance.getId());
                 step1 = processInstanceApi.getProcessInstanceVariable(processInstance.getId(), "Step1", false);
-            } catch (Throwable e) {
+            } catch (Throwable ignored) {
 
             }
         }
@@ -72,6 +76,123 @@ public class CommandService {
                 .status(HttpStatus.OK)
                 .body("Started process instance with id: " + processInstance.getId());
 
+    }
+
+    public ResponseEntity<String> startProcess2(String processDefinitionKey) throws ApiException {
+        log.info("Process : " + processDefinitionKey + " started");
+
+        UUID uuid = UUID.randomUUID();
+        HashMap<String, Object> vars = new HashMap<>();
+
+        // prepare variables to pass on to process
+        Map<String, VariableValueDto> variables = new HashMap<>();
+        variables.put("ex", new VariableValueDto().value("x").type("string"));
+
+        // start process instance
+        var processInstance = processDefinitionApi.startProcessInstanceByKey(
+                processDefinitionKey,
+                new StartProcessInstanceDto().variables(variables)
+        );
+        //
+
+        CompletableFuture<Boolean> futureStep1 = new CompletableFuture<>();
+
+        Runnable fetchStep1 = () -> {
+            int retries = 20;
+            while (!futureStep1.isDone()) {
+                try {
+                    Thread.sleep(500);
+                    if (extracted(processInstance.getId())) {
+                        futureStep1.complete(true);
+                        break;
+                    }
+                    retries--;
+                } catch (Throwable ignored) {
+                }
+                //
+                if (retries < 0) {
+                    log.error("Failed to retrieve Step1 variable after retries. Exiting.");
+                    futureStep1.completeExceptionally(new RuntimeException("Step1 retrieval timed out"));
+                    break;
+                }
+            }
+        };
+
+        new Thread(fetchStep1).start();
+
+        Boolean step1;
+        try {
+            step1 = futureStep1.get(10, TimeUnit.SECONDS);
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+            log.error("Error waiting for Step1 variable: " + e.getMessage(), e);
+            throw new ApiException("Failed to retrieve Step1 variable");
+        }
+
+        log.info("Process : " + processInstance.getId() + " ended");
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Started process instance with id: " + processInstance.getId());
+    }
+
+    private boolean extracted(String processInstance) throws ApiException {
+        var step1 = processInstanceApi.getProcessInstanceVariable(processInstance, "Step1", false);
+        if (step1 != null && (Objects.equals(step1.getValue(), "C") || Objects.equals(step1.getValue(), "F"))) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public Mono<ResponseEntity<String>> startProcessMono(String processDefinitionKey) throws ApiException {
+        log.info("Process : " + processDefinitionKey + " started");
+
+        UUID uuid = UUID.randomUUID();
+
+        // prepare variables to pass on to process
+        Map<String, VariableValueDto> variables = new HashMap<>();
+        variables.put("ex", new VariableValueDto().value("x").type("string"));
+        variables.put("Step1", new VariableValueDto().value("x").type("string"));
+
+        // start process instance
+        var processInstance = processDefinitionApi.startProcessInstanceByKey(
+                processDefinitionKey,
+                new StartProcessInstanceDto().variables(variables)
+        );
+        log.info("Process : " + processInstance.getId() + " ended");
+        //
+        return Mono.create(sink -> {
+            pollProcessInstanceStatus(processInstance.getId(), sink);
+        });
+    }
+
+    private void pollProcessInstanceStatus(String processInstanceId, MonoSink<ResponseEntity<String>> sink) {
+        boolean completed = false;
+        int retries = 10;
+        while (!completed) {
+            log.info("Time:" + System.currentTimeMillis());
+            retries--;
+            try {
+                if (extracted(processInstanceId)) {
+                    completed = true;
+                    sink.success(ResponseEntity
+                            .status(HttpStatus.OK)
+                            .body("Started process instance with id: " + processInstanceId)
+                    );
+                } else {
+                    Mono.delay(ofMillis(500))
+                            .subscribe(ignore -> {});
+                }
+            } catch (ApiException ignored) {
+            }
+            if (retries < 0) {
+                sink.success(ResponseEntity
+                        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .body("Error fetching process instance: Timeout")
+                );
+                completed = true;
+            }
+        }
     }
 
     public void sendMessage(String instanceId, String messageName) {
