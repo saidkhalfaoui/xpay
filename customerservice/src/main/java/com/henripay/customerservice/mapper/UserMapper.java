5a7de97f5c1ff7f04d5807ff4bf786aae371d937
Abdulrahim Djebablia <ADjebablia@aas.com.sa>
2024-03-17 14:48:47 +0100

    feat: create user delegator

--- customerservice/src/main/java/com/henripay/customerservice/mapper/UserMapper.java
+++ customerservice/src/main/java/com/henripay/customerservice/mapper/UserMapper.java
@@ -10,17 +10,12 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 @Component
 public interface UserMapper {
-    @Mapping(target = "merchantId", ignore = true)
-    @Mapping(target = "metadata.metadataId", source = "metadata")
-    @Mapping(target = "aggregator.aggregatorId", source = "aggregator")
+    @Mapping(target = "customerIdIdentifier", ignore = true)
     UserEntity toEntity(UserDTO source);
     List<UserEntity> toEntityList(List<UserDTO> source);
-    @Mapping(source = "metadata.metadataId", target = "metadata")
-    @Mapping(source = "aggregator.aggregatorId", target = "aggregator")
+
     UserDTO toDto(UserEntity source);
     List<UserDTO> toDtoList(List<UserEntity> source);
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mapping(target = "metadata.metadataId", source = "metadata")
-    @Mapping(target = "aggregator.aggregatorId", source = "aggregator")
     void updateFromDto(UserDTO dto, @MappingTarget UserEntity entity);
 }
