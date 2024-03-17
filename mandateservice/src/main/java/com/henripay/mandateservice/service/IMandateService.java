d6c98d146d8e6fd8e06cf5eb771a76ba743f10cb
Abdulrahim Djebablia <ADjebablia@aas.com.sa>
2024-03-17 17:30:42 +0100

    feat: add mandate mapper

--- mandateservice/src/main/java/com/henripay/mandateservice/service/IMandateService.java
+++ mandateservice/src/main/java/com/henripay/mandateservice/service/IMandateService.java
@@ -1,4 +1,15 @@
 package com.henripay.mandateservice.service;
 
+import com.henripay.domainservice.entity.MandateEntity;
+
+import java.util.List;
+
 public interface IMandateService {
+
+    MandateEntity saveMandate(MandateEntity mandate);
+    MandateEntity getMandateById(Long id);
+    List<MandateEntity> getAllMandates();
+    void deleteMandate(Long id);
+    MandateEntity updateMandateById(Long id, MandateEntity MandateEntity);
+    MandateEntity findByIban(String iban);
 }
--- mandateservice/src/main/java/com/henripay/mandateservice/service/Impl/MandateService.java
+++ mandateservice/src/main/java/com/henripay/mandateservice/service/Impl/MandateService.java
@@ -1,6 +1,49 @@
 package com.henripay.mandateservice.service.Impl;
 
+import com.henripay.domainservice.entity.MandateEntity;
+import com.henripay.domainservice.repository.MandateRepository;
+import com.henripay.domainservice.repository.MerchantRepository;
+import com.henripay.mandateservice.mapper.MandateMapper;
 import com.henripay.mandateservice.service.IMandateService;
 
+import java.util.List;
+
 public class MandateService implements IMandateService {
+    private final MandateRepository merchantRepository;
+    private final MandateMapper mandateMapper;
+
+    public MandateService(MandateRepository merchantRepository, MandateMapper mapper){
+        this.mandateMapper = mapper;
+        this.merchantRepository = merchantRepository;
+    }
+
+    @Override
+    public MandateEntity saveMandate(MandateEntity mandate) {
+        return null;
+    }
+
+    @Override
+    public MandateEntity getMandateById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<MandateEntity> getAllMandates() {
+        return null;
+    }
+
+    @Override
+    public void deleteMandate(Long id) {
+
+    }
+
+    @Override
+    public MandateEntity updateMandateById(Long id, MandateEntity MandateEntity) {
+        return null;
+    }
+
+    @Override
+    public MandateEntity findByIban(String iban) {
+        return null;
+    }
 }
