6682ccc3f8a8438af10ae5dd89c77527979a40b6
Abdulrahim Djebablia <ADjebablia@aas.com.sa>
2024-03-19 18:17:11 +0100

    feat: add exception handling mechanisme for mandate service

--- mandateservice/src/main/java/com/henripay/mandateservice/config/ExceptionTranslator.java
+++ mandateservice/src/main/java/com/henripay/mandateservice/config/ExceptionTranslator.java
@@ -1,24 +1,22 @@
-package com.henripay.customerservice.config;
+package com.henripay.mandateservice.config;
 
-import com.henripay.customerservice.dto.Error;
-import com.henripay.customerservice.exception.UserNotFoundException;
+import com.henripay.mandateservice.dto.Error;
+import com.henripay.mandateservice.exception.NoValidInput;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-/*
- *  @author: DevProblems(Sarang Kumar A Tak)
- *  Youtube channel: https://youtube.com/@devproblems
- */
+import javax.ws.rs.BadRequestException;
+
+
 @ControllerAdvice
 public class ExceptionTranslator {
 
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<Error> userNotFoundException(final UserNotFoundException e) {
-        return ResponseEntity.ok(getError(e.getMessage(), (long) HttpStatus.NOT_FOUND.value()));
+    @ExceptionHandler(NoValidInput.class)
+    public ResponseEntity<Error> invalidInputValidation(final NoValidInput e) {
+        return   ResponseEntity.status(HttpStatus.BAD_REQUEST).body(getError(e.getMessage(), (long) HttpStatus.NOT_FOUND.value()));
     }
-
 
     private Error getError(String message, Long code) {
         Error error = new Error();
--- mandateservice/src/main/java/com/henripay/mandateservice/service/Impl/MandateService.java
+++ mandateservice/src/main/java/com/henripay/mandateservice/service/Impl/MandateService.java
@@ -7,6 +7,7 @@ import com.henripay.domainservice.repository.MandateRepository;
 import com.henripay.domainservice.repository.MerchantRepository;
 import com.henripay.domainservice.repository.UserRepository;
 import com.henripay.mandateservice.dto.MandateDTO;
+import com.henripay.mandateservice.exception.NoValidInput;
 import com.henripay.mandateservice.mapper.MandateMapper;
 import com.henripay.mandateservice.service.IMandateService;
 import org.springframework.stereotype.Service;
@@ -31,8 +32,12 @@ public class MandateService implements IMandateService {
 
     @Override
     public MandateDTO saveMandate(MandateDTO mandateDTO) {
-        if(!merchantRepository.existsById(mandateDTO.getMerchant()) || !userRepository.existsById(mandateDTO.getCustomer())){
-            throw new BadRequestException("Bad Request");
+
+        if(!merchantRepository.existsById(mandateDTO.getMerchant())){
+            throw new NoValidInput("Invalid Merchant");
+        }
+        if (!userRepository.existsById(mandateDTO.getCustomer())){
+            throw new NoValidInput("Invalid Customer");
         }
         UserEntity customer = UserEntity.builder().customerIdIdentifier(mandateDTO.getCustomer()).build();
         MerchantEntity merchant = MerchantEntity.builder().merchantId(mandateDTO.getMerchant()).build();
@@ -49,7 +54,7 @@ public class MandateService implements IMandateService {
     public MandateDTO getMandateById(Long id) {
         return mandateMapper.toDto(
                 mandateRepository.findById(id)
-                        .orElseThrow(() -> new NotFoundException("Mandate not found")));
+                        .orElseThrow(() -> new NoValidInput("Invalid Mandate Id")));
     }
 
     @Override
@@ -59,16 +64,21 @@ public class MandateService implements IMandateService {
 
     @Override
     public void deleteMandate(Long id) {
+        mandateRepository.findById(id)
+                .orElseThrow(() -> new NoValidInput("Invalid Mandate Id"));
         mandateRepository.deleteById(id);
     }
 
     @Override
     public MandateDTO updateMandateById(Long id, MandateDTO mandateDTO) {
-        if(!merchantRepository.existsById(mandateDTO.getMerchant()) || !userRepository.existsById(mandateDTO.getCustomer())){
-            throw new BadRequestException("Bad Request");
+        if(!merchantRepository.existsById(mandateDTO.getMerchant())){
+            throw new NoValidInput("Invalid Merchant");
+        }
+        if (!userRepository.existsById(mandateDTO.getCustomer())){
+            throw new NoValidInput("Invalid Customer");
         }
         MandateEntity mandate = mandateRepository.findById(id)
-                .orElseThrow(() -> new BadRequestException("Bad Request"));
+                .orElseThrow(() -> new NoValidInput("Invalid Mandate Id"));
         mandateMapper.updateFromDto(mandateDTO, mandate);
         MandateEntity saveMandate = mandateRepository.save(mandate);
         return mandateMapper.toDto(saveMandate);
@@ -76,6 +86,5 @@ public class MandateService implements IMandateService {
 
     public boolean isMandateExists(UserEntity customer, MerchantEntity merchant) {
         return mandateRepository.existsByCustomerAndMerchant(customer, merchant);
-
     }
 }
