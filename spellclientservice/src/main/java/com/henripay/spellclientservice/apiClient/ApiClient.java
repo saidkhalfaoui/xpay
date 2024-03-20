49019bf5f7efe9be34e029041be829ef4ea7627d
MessaoudAzidane <fm_azidane@esi.dz>
2024-03-20 17:32:33 +0100

    deleted commented code

--- spellclientservice/src/main/java/com/henripay/spellclientservice/apiClient/ApiClient.java
+++ spellclientservice/src/main/java/com/henripay/spellclientservice/apiClient/ApiClient.java
@@ -9,6 +9,4 @@ import java.io.IOException;
 @Component
 public interface ApiClient {
      <T> Object makeCall(HttpMethod httpMethod, String url, T params, String apiKey) throws IOException;
-
-    //Object makeCall(HttpMethod httpMethod, String url, DirectPostRequestDto params, String apiKey) throws IOException;
 }
