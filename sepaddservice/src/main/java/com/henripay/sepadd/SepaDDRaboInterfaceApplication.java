1267b434538ba08e263174acc7f26c9af2c3a1a9
Mohammed Naami <m.naami@outlook.com>
2024-04-05 10:15:31 +0100

    HEN-9

--- sepaddservice/src/main/java/com/henripay/sepadd/SepaDDRaboInterfaceApplication.java
+++ sepaddservice/src/main/java/com/henripay/sepadd/SepaDDRaboInterfaceApplication.java
@@ -8,8 +8,7 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
-@SpringBootApplication
-
+@SpringBootApplication(scanBasePackages = "com.henripay")
 public class SepaDDRaboInterfaceApplication {
 
     public static void main(String[] args) {
